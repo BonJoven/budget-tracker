ed44e87cbebc31b22229915f5dc11153495870b7
Joven Montañez <joven.montanez06@gmail.com>
2026-08-09 23:34:20 +0800

    Update config.js

--- config.js
+++ config.js
@@ -4,5 +4,5 @@
 //   - SUPABASE_URL   = "Project URL"
 //   - SUPABASE_ANON_KEY = "anon public" key
 // ============================================================
-window.SUPABASE_URL =https://obabksylxiioijgnfswu.supabase.co/rest/v1/;
+window.SUPABASE_URL = https://obabksylxiioijgnfswu.supabase.co/rest/v1/;
 window.SUPABASE_ANON_KEY = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9iYWJrc3lseGlpb2lqZ25mc3d1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYyODM5NjAsImV4cCI6MjEwMTg1OTk2MH0.3UQ4JLCIqI3v-om6JJLrj4isJCr6dcQQBq65zGAi13E;
