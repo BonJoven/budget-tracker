d8f10de2942d741e99d2bbb03851bf9747ca0c5e
Joven Montañez <joven.montanez06@gmail.com>
2026-08-10 00:41:59 +0800

    Update config.js

--- config.js
+++ config.js
@@ -4,5 +4,5 @@
 //   - SUPABASE_URL   = "https://obabksylxiioijgnfswu.supabase.co/"
 //   - SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9iYWJrc3lseGlpb2lqZ25mc3d1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYyODM5NjAsImV4cCI6MjEwMTg1OTk2MH0.3UQ4JLCIqI3v-om6JJLrj4isJCr6dcQQBq65zGAi13E" key
 // ============================================================
-window.SUPABASE_URL = "https://obabksylxiioijgnfswu.supabase.co";
+window.SUPABASE_URL = "https://obabksylxiioijgnfswu.supabase.co/";
 window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9iYWJrc3lseGlpb2lqZ25mc3d1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYyODM5NjAsImV4cCI6MjEwMTg1OTk2MH0.3UQ4JLCIqI3v-om6JJLrj4isJCr6dcQQBq65zGAi13E";
